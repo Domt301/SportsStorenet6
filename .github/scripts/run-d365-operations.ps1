689f5cf404b2ec76f6509514d2cc068767e5fbf1
Leonardo Wildt <leonardod.trimarchi@gmail.com>
2023-06-27 13:21:27 -0400

    changed ps1

--- .github/scripts/run-d365-operations.ps1
+++ .github/scripts/run-d365-operations.ps1
@@ -1,9 +1,13 @@
+# Install Node.js
+Invoke-WebRequest -Uri "https://nodejs.org/dist/v14.15.1/node-v14.15.1-x64.msi" -OutFile "nodejs.msi"
+Start-Process -FilePath "msiexec.exe" -ArgumentList "/i nodejs.msi /passive" -Wait
+RefreshEnv
+
 # Install the Power Apps CLI
-Invoke-WebRequest -Uri "https://aka.ms/PowerAppsCLI" -OutFile ".\pac.exe"
-.\pac.exe
+npm install -g @microsoft/powerapps-cli
 
 # Authenticate with source environment
-pac auth create --url $env:SOURCE_ENV_URL --username $env:SOURCE_USERNAME --password $env:SOURCE_PASSWORD
+pac auth create --kind CDS --url $env:SOURCE_ENV_URL --username $env:SOURCE_USERNAME --password $env:SOURCE_PASSWORD
 
 # Publish all customizations
 pac org publishall
@@ -12,7 +16,7 @@ pac org publishall
 pac solution export --path .\solution.zip --name $env:SOLUTION_NAME
 
 # # Authenticate with target environment
-# pac auth create --url $env:TARGET_ENV_URL --username $env:TARGET_USERNAME --password $env:TARGET_PASSWORD
+# pac auth create --kind CDS --url $env:TARGET_ENV_URL --username $env:TARGET_USERNAME --password $env:TARGET_PASSWORD
 
 # # Import solution
 # pac solution import --path .\solution.zip
